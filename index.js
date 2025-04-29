d92c67b746dc61e79013f042316c53a227e9092a
Gustavo Nagai Amorim <gulifiree@gmail.com>
2025-04-29 20:50:31 -0300

    aprendendo a integrar o express.js com o EJS

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const app = express()
 
+app.set('view engine', 'ejs')  // estou dizendo para o express usar o EJS como View Engine
+
 app.get('/', (req, res)=> {
-    res.send('Bem vindo ao meu site!')
+    res.render('index')     // ao invés de utilizar o res.send(), utiliza-se o res.render() e passa o arquivo dentro dos parênteses, não precisa especificar a extensão. É dessa forma que o express fará a renderização do arquivo .ejs
+})
+
+app.get('/perfil', (req, res)=> {
+    res.render('principal/perfil')
 })
 
 app.listen(8080, ()=> {
